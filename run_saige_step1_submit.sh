a7a9282d15996c5f27c0c4cdb7ddb1d3ad117140
Annfeng <yfeng@broadinstitute.org>
2021-02-18 12:59:12 -0500

    update scripts

--- run_saige_step1_submit.sh
+++ run_saige_step1_submit.sh
@@ -26,25 +26,26 @@ done
 ####
 # Stratified analysis
 pop=eur;ana=c1
-strata=female
-strata=male
 covars=$(echo "age,age2,pc1,pc2,pc3,pc4,pc5,pc6,pc7,pc8,pc9,pc10")
-qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+for strata in female male; do 
+	qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+done
 
-strata=age.le.60
-strata=age.gt.60
 covars=$(echo "sex,age,age2,agexsex,pc1,pc2,pc3,pc4,pc5,pc6,pc7,pc8,pc9,pc10")
-qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+for strata in age.le.60 age.gt.60; do
+	qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+done
+
 
 
 ####
 pop=eur;ana=c2
-strata=female
-strata=male
 covars=$(echo "age,age2,pc1,pc2,pc3,pc4,pc5,pc6,pc7,pc8,pc9,pc10")
-qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+for strata in female male; do 
+	qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+done
 
-strata=age.le.60
-strata=age.gt.60
 covars=$(echo "sex,age,age2,agexsex,pc1,pc2,pc3,pc4,pc5,pc6,pc7,pc8,pc9,pc10")
+for strata in age.le.60 age.gt.60; do
 qsub -j y -l h_vmem=10g -l h_rt=15:00:00 -o $log/20200915_${pop}_saige_step1_${ana}-${strata}.out $scr/run_saige_step1-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${strata} ${covars}
+done
--- run_saige_step2-submit.sh
+++ run_saige_step2-submit.sh
@@ -2,24 +2,30 @@
 scr=/psych/genetics_data/yfeng/covid_hg/scripts
 log=/psych/genetics_data/yfeng/covid_hg/logs/
 outdir=/psych/genetics_data/yfeng/covid_hg/results/20200915
+# mkdir $outdir/parts #do this once
 
 #####
 # EUR
 
 # autosome
 indir=/psych/genetics_data/yfeng/covid_hg/genetic_data/eur_rel
-pop=eur;ana=c1;n_parts=100
-pop=eur;ana=c2;n_parts=100
-for chr in {22..1}; do
-    qsub -j y -l h_vmem=10g -l h_rt=24:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+pop=eur
+n_parts=100
+
+for ana in c1 c2; do
+    for chr in {22..1}; do
+        qsub -j y -l h_vmem=10g -l h_rt=24:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+    done
 done
 
 # chrX
 indir=/psych/genetics_data/yfeng/covid_hg/genetic_data/eur_rel
-pop=eur;ana=c1;n_parts=100
-pop=eur;ana=c2;n_parts=100
+pop=eur
+n_parts=100
 chr=X
-qsub -j y -l h_vmem=10g -l h_rt=24:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+for ana in c1 c2; do
+    qsub -j y -l h_vmem=10g -l h_rt=24:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+done
 
 
 
@@ -29,18 +35,23 @@ qsub -j y -l h_vmem=10g -l h_rt=24:00:00 -t 1-${n_parts} -o $log/20200915_${pop}
 
 # autosome
 indir=/humgen/diabetes2/users/josep/PartnersBIOBANK/merge_all_35K_datasets/merge_35K_genotypes/AFR/imputation_TOPMED_R2_RELATED_AFR/
-pop=afr;ana=c1;n_parts=100
-pop=afr;ana=c2;n_parts=100
-for chr in {22..1}; do
-    qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+pop=afr
+n_parts=100
+
+for ana in c1 c2; do
+    for chr in {22..1}; do
+        qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+    done
 done
 
 # chrX
 indir=/psych/genetics_data/yfeng/covid_hg/genetic_data/afr_rel
-pop=afr;ana=c1;n_parts=100
-pop=afr;ana=c2;n_parts=100
+pop=afr
+n_parts=100
 chr=X
-qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+for ana in c1 c2; do
+    qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+done
 
 
 
@@ -49,18 +60,23 @@ qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_
 
 # autosome
 indir=/humgen/diabetes2/users/josep/PartnersBIOBANK/merge_all_35K_datasets/merge_35K_genotypes/HIS/imputation_TOPMED_R2_RELATED_HIS/
-pop=his;ana=c1;n_parts=100
-pop=his;ana=c2;n_parts=100
-for chr in {22..1}; do
-    qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+pop=his
+n_parts=100
+
+for ana in c1 c2; do
+    for chr in {22..1}; do
+        qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+    done
 done
 
 # chrX
 indir=/psych/genetics_data/yfeng/covid_hg/genetic_data/his_rel/
-pop=his;ana=c1;n_parts=100
-pop=his;ana=c2;n_parts=100
+pop=his
+n_parts=100
 chr=X
-qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}-test.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+for ana in c1 c2; do
+    qsub -j y -l h_vmem=10g -l h_rt=8:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}-test.out $scr/run_saige_step2.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts}
+done
 
 
 
@@ -76,14 +92,18 @@ outdir=/psych/genetics_data/yfeng/covid_hg/results/20200915
 # EUR
 indir=/psych/genetics_data/yfeng/covid_hg/genetic_data/eur_rel
 pop=eur;ana=c1;n_parts=100
+strata=male
+strata=female
+strata=age.le.60
+strata=age.gt.60
+
 pop=eur;ana=c2;n_parts=100
 strata=male
 strata=female
 strata=age.le.60
 strata=age.gt.60
 
-# for chr in {22..1}; do
-for strata in male female age.le.60 age.gt.60; do 
+for chr in {22..1}; do
     qsub -j y -l h_vmem=10g -l h_rt=24:00:00 -t 1-${n_parts} -o $log/20200915_${pop}_saige_step2_${ana}-${strata}.out $scr/run_saige_step2-stratified.sh ${indir} ${outdir} ${pop} ${ana} ${chr} ${n_parts} ${strata}
 done
 
