4c5da66867c1c3551534a4b8f42b3a6fcea5769c
Annfeng <yfeng@broadinstitute.org>
2021-03-09 12:24:21 -0500

    update script

--- upload_results.sh
+++ upload_results.sh
@@ -1,7 +1,10 @@
 
 # Upload the results to the designated google cloud bucket
+qlogin -q interactive -l h_vmem=20g -l h_rt=10:00:00
 
-cd /psych/genetics_data/yfeng/covid_hg/results/20200915
-gsutil -m cp *AFR*gz gs://covid19-hg-upload-partnersbiobank
-gsutil -m cp *HIS*gz gs://covid19-hg-upload-partnersbiobank
-gsutil -m cp *EUR*gz gs://covid19-hg-upload-partnersbiobank 
+
+# cd /psych/genetics_data/yfeng/covid_hg/results/20200915
+cd /psych/genetics_data/yfeng/covid_hg/results/20210301
+gsutil -m cp *PHBB*AFR*gz gs://covid19-hg-upload-partnersbiobank
+gsutil -m cp *PHBB*HIS*gz gs://covid19-hg-upload-partnersbiobank
+gsutil -m cp *PHBB*EUR*gz gs://covid19-hg-upload-partnersbiobank 
