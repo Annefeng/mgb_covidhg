6ac50217aeac42e90f62b13fce1023a6819e7d3b
Annfeng <yfeng@broadinstitute.org>
2021-03-09 12:23:56 -0500

    update script

--- cat_saige_results-submit.sh
+++ cat_saige_results-submit.sh
@@ -1,35 +1,33 @@
 
 scr=/psych/genetics_data/yfeng/covid_hg/scripts
 log=/psych/genetics_data/yfeng/covid_hg/logs/
-wdir=/psych/genetics_data/yfeng/covid_hg/results/20200915/
-date=20200915
+# wdir=/psych/genetics_data/yfeng/covid_hg/results/20200915/
+# date=20200915
+wdir=/psych/genetics_data/yfeng/covid_hg/results/20210301
+date=20210301
 ###
 pop=eur
-ana=c1;iter=4;ncase=151;nctrl=3118;strata=all
-ana=c1;iter=4;ncase=70;nctrl=1371;strata=male
-ana=c1;iter=4;ncase=81;nctrl=1747;strata=female
-ana=c1;iter=4;ncase=81;nctrl=1331;strata=age.le.60
-ana=c1;iter=4;ncase=70;nctrl=1787;strata=age.gt.60
-qsub -o $log/cat_results_$pop_$ana.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
+ana=b1;iter=6;ncase=112;nctrl=303;strata=all
+qsub -o $log/20210301_cat_results_${pop}_${ana}.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
 
+ana=b2;iter=6;ncase=112;nctrl=29702;strata=all
+qsub -o $log/20210301_cat_results_${pop}_${ana}.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
 
-ana=c2;iter=4;ncase=151;nctrl=29966;strata=all
-ana=c2;iter=4;ncase=70;nctrl=14179;strata=male
-ana=c2;iter=4;ncase=81;nctrl=15787;strata=female
-ana=c2;iter=4;ncase=81;nctrl=13042;strata=age.le.60
-ana=c2;iter=4;ncase=70;nctrl=16924;strata=age.gt.60
-qsub -o $log/cat_results_$pop_$ana.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
+ana=c2;iter=6;ncase=415;nctrl=29702;strata=all
+ana=c2;iter=6;ncase=70;nctrl=14179;strata=male
+ana=c2;iter=6;ncase=81;nctrl=15787;strata=female
+ana=c2;iter=6;ncase=81;nctrl=13042;strata=age.le.60
+ana=c2;iter=6;ncase=70;nctrl=16924;strata=age.gt.60
+qsub -o $log/20210301_cat_results_${pop}_${ana}.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
 
 
 ###
 pop=afr
-ana=c1;iter=4;ncase=60;nctrl=375
-ana=c2;iter=4;ncase=60;nctrl=2445
-qsub -o $log/cat_results_$pop_$ana.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date
+ana=c2;iter=6;ncase=127;nctrl=2378;strata=all
+qsub -o $log/20210301_cat_results_${pop}_${ana}.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
 
 ###
 pop=his
-ana=c1;iter=4;ncase=66;nctrl=276
-ana=c2;iter=4;ncase=66;nctrl=2405
-qsub -o $log/cat_results_$pop_$ana.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date
+ana=c2;iter=6;ncase=123;nctrl=2348;strata=all
+qsub -o $log/20210301_cat_results_${pop}_${ana}.out $scr/cat_saige_results.sh $wdir $pop $ana $iter $ncase $nctrl $date $strata
 
